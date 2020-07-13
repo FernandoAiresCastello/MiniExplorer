2fbc64e033b5c4b200e5e8c17b7c51dcdf61d8e0
Fernando <fernandoairescastello@gmail.com>
2020-07-13 00:13:04 -0300

    Application icon

---
+++ MiniExplorer/SpecialDirectoryInfo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniExplorer
+{
+    public class SpecialDirectoryInfo
+    {
+        public string Name { get; private set; }
+        public string Path { get; private set; }
+
+        public SpecialDirectoryInfo(string name, string path)
+        {
+            Name = name;
+            Path = path;
+        }
+    }
+}
